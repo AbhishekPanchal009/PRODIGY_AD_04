efc7550995eb3cd69f1c20db6a51a13a86891f9a
Abhishekpanchal <panchalabhishek2506@gmail.com>
2026-05-09 12:31:36 +0530

    final draft

--- lib/main.dart
+++ lib/main.dart
@@ -32,48 +32,126 @@ class _TicTacToeState extends State<TicTacToe>{
     if(board[index] == "" && winner == ""){
       setState(() {
         board[index] = currentPlayer;
+
+        checkWinner();
         currentPlayer = currentPlayer == "X" ? "O" : "X";
       });
     }
+  }
+  void checkWinner() {
+    List<List<int>> patterns = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ];
+
+    for (var p in patterns) {
+      String a = board[p[0]];
+      String b = board[p[1]];
+      String c = board[p[2]];
+
+      if (a != "" && a == b && b == c) {
+        winner = a;
+        return;
+      }
+    }
+
+    // Draw condition
+    if (!board.contains("")) {
+      winner = "Draw";
+    }
+  }
+  void resetGame() {
+    setState(() {
+      board = List.generate(9, (index) => "");
+      currentPlayer = "X";
+      winner = "";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('Tic Tac Toe'),
+        backgroundColor: Colors.black,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Tic Tac Toe",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Turn: $currentPlayer",
-          style: TextStyle(fontSize: 24),
+          Text(
+      winner == ""
+      ? "Turn: $currentPlayer"
+          : winner == "Draw"
+      ? "It's a Draw!"
+        : "Winner: $winner",
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        )
           ),
           SizedBox(height: 20),
-          GridView.builder(
-            shrinkWrap: true,
-            itemCount: 9,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-            ),
-            itemBuilder: (context,index){
-              return GestureDetector(
-                onTap: () =>handleTap(index),
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black),
-                  ),
-                  child: Center(
-                    child: Text(
-                      board[index],
-                      style: TextStyle(fontSize: 40),
+          Center(
+            child: SizedBox(
+              height: 320,
+              width: 320,
+              child: GridView.builder(
+              shrinkWrap: true,
+              itemCount: 9,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+              ),
+              itemBuilder: (context,index){
+                return GestureDetector(
+                  onTap: () =>handleTap(index),
+                   child: Container(
+                    margin: EdgeInsets.all(6),
+                      decoration: BoxDecoration(color: Colors.grey[900],
+                                 borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                           BoxShadow(
+                                    color: Colors.black54,
+                          blurRadius: 4,
+                             offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+
+                child: Center(
+                  child: Text(
+                  board[index],
+                  style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: board[index] == "X"
+                    ? Colors.cyan
+                    : Colors.pinkAccent,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          )
+                );
+              },
+            ),
+            ),
+          ),
+          SizedBox(height: 20),
+
+          ElevatedButton(
+            onPressed: resetGame,
+            child: Text("Restart Game"),
+          ),
         ],
       ),
     );
